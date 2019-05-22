62d835de94743f081782fa1853f5f3e567c27913
Cody Bumgardner <cody@uky.edu>
2019-05-22 15:19:17 -0400

    update lib

--- src/main/java/io/cresco/core/Activator.java
+++ src/main/java/io/cresco/core/Activator.java
@@ -136,7 +136,7 @@ public final class Activator
 
         log4jProps.put( "log4j.category.org.apache.felix","ERROR");
         log4jProps.put( "log4j.category.org.ops4j.pax","ERROR");
-        log4jProps.put( "log4j.category.com.orientechnologies","ERROR");
+        //log4jProps.put( "log4j.category.com.orientechnologies","ERROR");
         log4jProps.put( "log4j.category.io.netty","ERROR");
         log4jProps.put( "log4j.category.org.hibernate","ERROR");
         log4jProps.put( "log4j.category.org.apache.activemq","ERROR");
