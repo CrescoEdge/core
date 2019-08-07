e8c5593660156a8433cfbc7c8533fd324176d939
Cody Bumgardner <cody@uky.edu>
2019-08-07 10:29:43 -0400

    come on!!! 2

--- src/test/java/io/cresco/core/ActivatorTest.java
+++ src/test/java/io/cresco/core/ActivatorTest.java
@@ -1,6 +1,10 @@
 package io.cresco.core;
 
+import org.apache.sling.testing.mock.osgi.MockOsgi;
 import org.junit.jupiter.api.Test;
+import org.osgi.framework.BundleContext;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class ActivatorTest {
@@ -9,8 +13,17 @@ class ActivatorTest {
     @Test
     void start() {
 
-        int i = 0;
-        i += 1;
+        try {
+            // get bundle context
+            BundleContext bundleContext = MockOsgi.newBundleContext();
+
+            Activator activator = new Activator();
+            activator.start(bundleContext);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        //assertTrue(true);
+
 
     }
 
@@ -19,6 +32,8 @@ class ActivatorTest {
 
         int i = 0;
         i += 1;
+
+        assertTrue(true);
 
     }
 }
